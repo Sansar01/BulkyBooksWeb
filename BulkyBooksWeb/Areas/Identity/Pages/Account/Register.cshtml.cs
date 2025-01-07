7267ec3f5726e045fa13754b6877d4a257b009a2
Sansar Tiwari <st9889477@gmail.com>
2025-01-07 21:13:40 +0530

    register

--- BulkyBooksWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyBooksWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -110,6 +110,9 @@ namespace BulkyBooksWeb.Areas.Identity.Pages.Account
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get;set; }
 
+
+            [Required]
+            public string Name { get; set; }
             public string? StreetAddress { get; set; }
             public string? City { get; set; }
             public string? State { get; set; }
@@ -150,6 +153,13 @@ namespace BulkyBooksWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+                
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PhoneNumber = Input.PhoneNumber;
+                user.PostalCode = Input.PostalCode;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
