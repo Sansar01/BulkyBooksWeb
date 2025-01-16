be8aa5cf0a682dfe36d22053e66580691b00a540
Sansar Tiwari <st9889477@gmail.com>
2025-01-16 17:07:46 +0530

    order confirmation done

--- Bulky.Utilty/SD.cs
+++ Bulky.Utilty/SD.cs
@@ -7,8 +7,6 @@
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
 
-
-
         public const string StatusPending = "Pending";
         public const string StatusApproved = "Approved";
         public const string StatusInProcess = "Processing";
--- BulkyBooksWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBooksWeb/Areas/Customer/Controllers/CartController.cs
@@ -73,7 +73,7 @@ namespace BulkyBooksWeb.Areas.Customer.Controllers
         }
 
         [HttpPost]
-        [ActionName("Name")]
+        [ActionName("Summary")]
         public IActionResult SummaryPost()
         {
             var claimsIdentity = (ClaimsIdentity)User.Identity;
@@ -85,7 +85,7 @@ namespace BulkyBooksWeb.Areas.Customer.Controllers
             ShoppingCartVM.OrderHeader.OrderDate = System.DateTime.Now;
             ShoppingCartVM.OrderHeader.ApplicationUserId = userId;
 
-            ShoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.Get(u=>u.Id == userId);
+           ApplicationUser applicationuser = _unitOfWork.ApplicationUser.Get(u=>u.Id == userId);
 
 
             foreach (var cart in ShoppingCartVM.ShoppingCartList)
@@ -93,7 +93,7 @@ namespace BulkyBooksWeb.Areas.Customer.Controllers
                 cart.Price = GetPriceBasedOnQuantity(cart);
                 ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
             }
-            if(ShoppingCartVM.OrderHeader.ApplicationUser.CompanyId.GetValueOrDefault() == 0)
+            if(applicationuser.CompanyId.GetValueOrDefault() == 0)
             {  // it is a regular customer account and we need to capture payment
                 ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending; 
                 ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusPending;
@@ -117,7 +117,18 @@ namespace BulkyBooksWeb.Areas.Customer.Controllers
                 _unitOfWork.OrderDetail.Add(orderDetail);
                 _unitOfWork.save();
             }
-            return View(ShoppingCartVM);
+
+            if (applicationuser.CompanyId.GetValueOrDefault() == 0)
+            {
+                //it is a regular customer account and we need to capture payment
+                //stripe logic
+            }
+            return RedirectToAction(nameof(OrderConfirmation),new {id = ShoppingCartVM.OrderHeader.Id});
+        }
+
+        public IActionResult OrderConfirmation(int id)
+        {
+            return View(id);
         }
 
         public IActionResult Plus(int cartId)
