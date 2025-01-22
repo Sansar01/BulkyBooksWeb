e8e59eb2b6b9b0900b9deb61e045bb5ec6c66318
Sansar Tiwari <st9889477@gmail.com>
2025-01-22 14:19:50 +0530

    updation

---
+++ Bulky.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,16 @@
+﻿using Bulky.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public IEnumerable<OrderHeader> OrderHeader { get; set; }  
+        
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
+    }
+}
---
+++ BulkyBooksWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,62 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.Models;
+using Bulky.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBooksWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public OrderController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            OrderVM orderVM = new()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList(),
+                OrderDetail = new List<OrderDetail>()
+            };
+            return View(orderVM);
+        }
+
+        #region Api calls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> obj = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = obj });
+        }
+
+        public IActionResult Deletes(int id)
+        {
+            var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, productToBeDeleted.ImageUrl.TrimStart('\\'));
+
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.Product.Remove(productToBeDeleted);
+            _unitOfWork.save();
+
+            return Json(new { success = true, message = "Delete successfull" });
+        }
+
+        #endregion
+    }
+}
