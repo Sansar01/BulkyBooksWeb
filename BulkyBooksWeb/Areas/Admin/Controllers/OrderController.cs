a3e89b0e9bebcf2896c0864be3ae8824e136e702
Sansar Tiwari <st9889477@gmail.com>
2025-01-23 14:01:33 +0530

    updation

--- BulkyBooksWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBooksWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 ﻿using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models.Models;
 using Bulky.Models.ViewModels;
+using Bulky.Utilty;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,46 +18,65 @@ namespace BulkyBooksWeb.Areas.Admin.Controllers
             _unitOfWork = unitOfWork;
             _webHostEnvironment = webHostEnvironment;
         }
-        public IActionResult Index()
+        public IActionResult Index(string? status)
         {
             OrderVM orderVM = new()
             {
                 OrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList(),
                 OrderDetail = new List<OrderDetail>()
             };
+
+            switch (status)
+            {
+                case "pending":
+                    orderVM.OrderHeader = orderVM.OrderHeader.Where(u => u.PaymentStatus == SD.PaymentStatusPending);
+                    break;
+                case "inprocess":
+                    orderVM.OrderHeader = orderVM.OrderHeader.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    orderVM.OrderHeader = orderVM.OrderHeader.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderVM.OrderHeader = orderVM.OrderHeader.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
             return View(orderVM);
         }
 
-        #region Api calls
+        //#region Api calls
 
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            List<OrderHeader> obj = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-            return Json(new { data = obj });
-        }
+        //[HttpGet]
+        //public IActionResult GetAll()
+        //{
+        //    List<OrderHeader> obj = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+        //    return Json(new { data = obj });
+        //}
 
-        public IActionResult Deletes(int id)
-        {
-            var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
-            if (productToBeDeleted == null)
-            {
-                return Json(new { success = false, message = "Error while deleting" });
-            }
+        //public IActionResult Deletes(int id)
+        //{
+        //    var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+        //    if (productToBeDeleted == null)
+        //    {
+        //        return Json(new { success = false, message = "Error while deleting" });
+        //    }
 
-            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, productToBeDeleted.ImageUrl.TrimStart('\\'));
+        //    var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, productToBeDeleted.ImageUrl.TrimStart('\\'));
 
-            if (System.IO.File.Exists(oldImagePath))
-            {
-                System.IO.File.Delete(oldImagePath);
-            }
+        //    if (System.IO.File.Exists(oldImagePath))
+        //    {
+        //        System.IO.File.Delete(oldImagePath);
+        //    }
 
-            _unitOfWork.Product.Remove(productToBeDeleted);
-            _unitOfWork.save();
+        //    _unitOfWork.Product.Remove(productToBeDeleted);
+        //    _unitOfWork.save();
 
-            return Json(new { success = true, message = "Delete successfull" });
-        }
+        //    return Json(new { success = true, message = "Delete successfull" });
+        //}
 
-        #endregion
+        //#endregion
     }
 }
